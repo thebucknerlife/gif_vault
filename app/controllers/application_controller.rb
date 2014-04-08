033941e255e8abb17e09b331dd3fc3dbfe073558
Greg Buckner <gbuckner23@gmail.com>
2014-04-08 12:29:43 -0700

    made a fix to code based on dickey's input - forgot to instaniate the current_user variable as an instance variable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
-    current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   helper_method :current_user
 
